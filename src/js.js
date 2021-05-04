87b42acde4583f7482137e353cc306802262043a
fesousadev <fesousalocutor@gmail.com>
2021-05-04 19:13:28 -0300

    update

---
+++ src/js.js
@@ -0,0 +1,22 @@
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+function lampadaQuebrada(){
+    return lamp.src.indexOf('quebrada') > -1
+};
+function lampadaLigada(){
+    if(!lampadaQuebrada()){
+        lamp.src='./img/acesa.jpg'
+    }
+};
+function lampadaDesligada(){
+    if(!lampadaQuebrada()){
+        lamp.src='./img/apagada.jpg'
+    }
+};
+function lampadaQuebrando(){
+    lamp.src='./img/quebrada.jpg'
+};
+turnOn.addEventListener('click', lampadaLigada);
+turnOff.addEventListener('click', lampadaDesligada);
+lamp.addEventListener('click', lampadaQuebrando);
